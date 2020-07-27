1dde27babd8e7bcff0da66ad9ce7fa0bb26c25f4
EC2 Default User <ec2-user@ip-172-31-54-184.ec2.internal>
2020-07-27 10:44:39 +0000

    env added

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const mongoose = require("mongoose");
 
-app.use("/", routes);
-const port = process.env.PORT || 3000;
-app.listen(port => console.log("app is running at port: ", port))
+const dotenv = require("dotenv")
+
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const app = express();
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(res => {
+    console.log("connected to mongoose instance");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use("/", routes);
+    const port = process.env.PORT || 3000;
+    app.listen(() => console.log("app is running at port: ", port))
+
+  })
+  .catch(er => console.log("failed to connect to mongoose instance"));
