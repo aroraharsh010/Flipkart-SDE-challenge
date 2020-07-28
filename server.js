8be11cc95cc9689c6d2e43b950da0352fd62894b
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-07-28 18:22:20 +0530

    fix

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const routes = require('./router')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routes = require("./router")
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
-  .then(res => {
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use("/", routes);
-    app.get("/test", (req, res) => res.status(200).send({ message: "pong" }));
-    const port = process.env.PORT || 8080
-;
-//	console.log({port});
-    app.listen( () => console.log("app is running at port:", port));
-//        console.log({port});
-
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .catch(er => console.log("failed to connect to mongoose instance"));
+  .then((res) => console.log("database connected"))
+  .catch((er) => console.log("failed to connect to mongoose instance"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", routes);
+app.get("/test", (req, res) => res.status(200).send({ message: "pong" }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
